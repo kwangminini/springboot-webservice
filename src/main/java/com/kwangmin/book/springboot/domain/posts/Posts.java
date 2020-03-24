9ccdace5415915b98e0e365236a04b88aa3be9c3
kwangminini <rhkdals7362@gmail.com>
2020-03-25 04:20:05 +0900

    posts 수정/조회 commit

--- src/main/java/com/kwangmin/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/kwangmin/book/springboot/domain/posts/Posts.java
@@ -28,4 +28,9 @@ public class Posts { //실제 DB의 테이블과 매칭될 클래스이며 보�
         this.content=content;
         this.author=author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
